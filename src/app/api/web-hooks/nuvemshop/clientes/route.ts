31f03c8c6e69c8fab309fc545a9a324e21a8c00b
Rafael <rafaelventurinidipalma@gmail.com>
2025-08-29 14:37:11 -0300

    adicionado tratamento de erro para entender o que está acontecendo

--- src/app/api/web-hooks/nuvemshop/clientes/route.ts
+++ src/app/api/web-hooks/nuvemshop/clientes/route.ts
@@ -4,15 +4,22 @@ import {
 } from "@/util/WebHook/nuvemshop/client/arrange-client";
 
 export async function POST(request: Request) {
-	const verified = await verifyPost(request)
-	
-	if (!verified) {
-		return new Response("Unauthorized", {status: 401});
+	try {
+		
+		const verified = await verifyPost(request)
+		
+		if (!verified) {
+			console.log("Erro ao verificar WebHook")
+			return new Response("Unauthorized", {status: 401});
+		}
+		const data = await request.json();
+		
+		await getClientById(data.id)
+		
+		
+		return new Response(`Webhook verified: ${verified}`, {status: 200});
+	} catch (e) {
+		console.log("Erro ao processar pedido: ", e)
+		return new Response(`Error on webhook`, {status: 500})
 	}
-	const data = await request.json();
-	
-	await getClientById(data.id)
-	
-	
-	return new Response(`Webhook verified: ${verified}`, {status: 200});
 }
--- src/app/api/web-hooks/nuvemshop/vendas/route.ts
+++ src/app/api/web-hooks/nuvemshop/vendas/route.ts
@@ -5,17 +5,23 @@ import {
 import {verifyPost} from "@/util/WebHook/nuvemshop/reusable";
 
 export async function POST(request: Request) {
-	const verified = await verifyPost(request)
-	
-	if (!verified) {
-		return new Response("Unauthorized", {status: 401});
+	try {
+		
+		const verified = await verifyPost(request)
+		if (!verified) {
+			console.log("Erro ao verificar WebHook")
+			return new Response("Unauthorized", {status: 401});
+		}
+		const data = await request.json();
+		const order = await getOrderById(data.id);
+		if (order.status === "ok") {
+			await processOrder(order.response);
+		}
+		return new Response(`Webhook verified: ${verified}`, {status: 200});
+	} catch (e) {
+		console.log("Erro ao processar pedido: ", e)
+		return new Response(`Error on webhook`, {status: 500})
 	}
-	const data = await request.json();
-	const order = await getOrderById(data.id);
-	if (order.status === "ok") {
-		await processOrder(order.response);
-	}
 	
-	return new Response(`Webhook verified: ${verified}`, {status: 200});
 }
 
