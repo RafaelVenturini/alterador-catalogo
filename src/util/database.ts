e296d3063ced4e7b9be74e25184cfdffe9d6026c
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-02-19 13:38:43 -0300

    atualizado credenciais do banco CORRETAS.

--- src/util/database.ts
+++ src/util/database.ts
@@ -4,11 +4,11 @@ import {config} from "dotenv"
 config()
 
 export const connection = mysql.createPool({
-	host: process.env.CATALOG_FIT_MYSQL_DB_HOST,
+	host: process.env.CHANGER_FIT_MYSQL_DB_HOST,
 	port: 3307,
-	user: process.env.CATALOG_FIT_MYSQL_DB_USER,
-	password: process.env.CATALOG_FIT_MYSQL_DB_PASSWORD,
-	database: process.env.CATALOG_FIT_MYSQL_DB_SCHEMA,
+	user: process.env.CHANGER_FIT_MYSQL_DB_USER,
+	password: process.env.CHANGER_FIT_MYSQL_DB_PASSWORD,
+	database: process.env.CHANGER_FIT_MYSQL_DB_SCHEMA,
 	waitForConnections: true,
 	idleTimeout: 300000,
 	connectionLimit: 12,
